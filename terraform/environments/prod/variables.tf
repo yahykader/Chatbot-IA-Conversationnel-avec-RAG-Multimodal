a4957f4a9fe063d77e2c61bedccc6edd0ea33c94
Yahyaoui <yahyaouiabdelkader85@gmail.com>
2026-02-25 11:58:50 +0100

    fix variables formatage

--- terraform/environments/prod/variables.tf
+++ terraform/environments/prod/variables.tf
@@ -1,5 +1,23 @@
-variable "project_id"   { type = string }
-variable "region"       { type = string  default = "europe-west1" }
-variable "zone"         { type = string  default = "europe-west1-b" }
-variable "vm_name"      { type = string  default = "rag-vm-prod" }
-variable "machine_type" { type = string  default = "e2-standard-4" }  # 4 vCPU, 16GB
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+  default = "europe-west1"
+}
+
+variable "zone" {
+  type    = string
+  default = "europe-west1-b"
+}
+
+variable "vm_name" {
+  type    = string
+  default = "rag-vm-prod"
+}
+
+variable "machine_type" {
+  type    = string
+  default = "e2-standard-4" # 4 vCPU, 16GB
+}
