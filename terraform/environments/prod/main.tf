2525bd51def1e9a60ab5f32723b4dcf2fc4c0eb5
Yahyaoui <yahyaouiabdelkader85@gmail.com>
2026-02-26 17:20:51 +0100

    fix iam

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -105,3 +105,7 @@ resource "google_project_iam_member" "cloudrun_ar_reader" {
   role    = "roles/artifactregistry.reader"
   member  = "serviceAccount:${data.google_project.project.number}-compute@developer.gserviceaccount.com"
 }
+
+data "google_project" "project" {
+  project_id = var.project_id
+}
